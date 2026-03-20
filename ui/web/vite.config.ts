ecc0c59975998fb594163fa4fe9f97d289f30b9e
nguyendondong <nguyendondong.dev@gmail.com>
2026-03-20 05:33:01 +0000

    overwrite port

--- ui/web/vite.config.ts
+++ ui/web/vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     server: {
-      port: 5173,
+      port: 51730,
       proxy: {
         "/ws": {
           target: `http://${backendHost}:${backendPort}`,
